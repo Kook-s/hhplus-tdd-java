2c5e202ffda316d3848ffec29a6df4540c5704a8
Kook-s <kooks1126@naver.com>
2024-09-25 01:35:59 +0900

    Changes

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,2 +1,70 @@
-package io.hhplus.tdd.point;public class PointService {
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Service
+public class PointService {
+
+    @Autowired
+    private UserPointTable userPointTable;
+    @Autowired
+    private PointHistoryTable pointHistoryTable;
+    private final Lock lock = new ReentrantLock();
+
+    public UserPoint chargePoint(long userId, long amount) {
+        UserPoint updatedUserPoint;
+         try {
+            lock.lock();
+
+            // 포인트 충전
+            updatedUserPoint = userPointTable.insertOrUpdate(userId, getCurrentPoint(userId) + amount);
+            // 포인트 충전 내역 기록
+            pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+        } finally {
+            lock.unlock();
+        }
+
+        return updatedUserPoint;
+    }
+
+    public UserPoint usePoint(long userId, long amount) {
+        UserPoint updatedUserPoint;
+        try {
+            lock.lock();
+            long currentPoint = getCurrentPoint(userId);
+            if (currentPoint < amount) {
+                throw new IllegalArgumentException("잔액이 부족합니다.");
+            }
+            // 포인트 사용
+            updatedUserPoint = userPointTable.insertOrUpdate(userId, currentPoint - amount);
+            // 포인트 사용 내역 기록
+            pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
+
+        } finally {
+            lock.unlock();
+        }
+        return updatedUserPoint;
+    }
+
+    public UserPoint getPoint(long userId) {
+        return userPointTable.selectById(userId);
+    }
+
+    public List<PointHistory> getHistory(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
+    }
+
+    private long getCurrentPoint(long userId) {
+        return userPointTable.selectById(userId).point();
+    }
+
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -1,4 +1,74 @@
-import static org.junit.jupiter.api.Assertions.*;
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+@DisplayName("포인트 서비스 로직 테스트")
+@SpringBootTest
 class PointServiceTest {
-  
+
+    @InjectMocks
+    private PointService pointService;
+
+    @Mock
+    private UserPointTable userPointTable;
+
+    @Mock
+    private PointHistoryTable pointHistoryTable;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("ID로 유저를 조회할 때 존재하는 경우")
+    public void selectUserByIdTest() throws Exception{
+        //given
+        long id = 1;
+        UserPoint userPoint = new UserPoint(id, 0, System.currentTimeMillis());
+        given(userPointTable.selectById(id)).willReturn(userPoint);
+
+        //when
+        UserPoint result = pointService.getPoint(id);
+
+        //then
+        assertThat(result.id()).isEqualTo(id);
+    }
+
+    @Test
+    public void chargePointTest() {
+        //given
+        long id = 1;
+        long amount = 50000;
+        long addAmount = 30000;
+        UserPoint userPoint = new UserPoint(id, amount, System.currentTimeMillis());
+
+        given(userPointTable.selectById(id)).willReturn(userPoint);
+        given(userPointTable.insertOrUpdate(anyLong(), anyLong())).willReturn(new UserPoint(id, userPoint.point()+addAmount, 0));
+
+        //when
+        UserPoint result = pointService.chargePoint(id, amount);
+
+        //then
+        assertThat(result.id()).isEqualTo(id);
+        assertThat(result.point()).isEqualTo(amount+addAmount);
+    }
+
 }
+
+
+
